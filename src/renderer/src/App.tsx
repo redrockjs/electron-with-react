ec014c4224dbcd2af16c7b2064b2a400de1ebeb9
RedRock <smb.sochi@gmail.com>
2023-08-27 21:19:45 +0300

    fix: update project to electon-vite react-ts

---
+++ src/renderer/src/App.tsx
@@ -0,0 +1,9 @@
+//import MainPage from './pages/MainPage/MainPage'
+import { ReactNode } from 'react'
+
+function App(): ReactNode {
+  // return <MainPage />
+  return <>TEST</>
+}
+
+export default App
