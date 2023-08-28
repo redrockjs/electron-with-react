34f51e0c39285f37d62c4b5ec34e78016529b6dd
redrockjs <smb.sochi@gmail.com>
2023-08-28 09:34:20 +0300

    fix: add iframe music
    Took 33 minutes

--- src/renderer/src/pages/MusicPage/MusicPage.tsx
+++ src/renderer/src/pages/MusicPage/MusicPage.tsx
@@ -1,7 +1,15 @@
 import { ReactNode } from 'react'
 
 function MusicPage(): ReactNode {
-  return <>Music</>
+  return (
+    <div>
+      <webview
+        id="foo"
+        src="https://music.yandex.ru"
+        style={{display:'flex', width:'100%', height:'calc(100vh - 60px)'}}
+      ></webview>
+    </div>
+  )
 }
 
 export default MusicPage
