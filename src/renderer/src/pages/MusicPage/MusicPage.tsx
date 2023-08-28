d4817fbf99461d548c7b88c7bcb8ac52a093e471
RedRock <smb.sochi@gmail.com>
2023-08-28 22:34:57 +0300

    fix: try insertCSS

--- src/renderer/src/pages/MusicPage/MusicPage.tsx
+++ src/renderer/src/pages/MusicPage/MusicPage.tsx
@@ -4,7 +4,7 @@ function MusicPage(): ReactNode {
   return (
     <div>
       <webview
-        id="foo"
+        id="music"
         src="https://music.yandex.ru"
         style={{ display: 'flex', width: '100%', height: 'calc(100vh - 60px)' }}
       ></webview>
